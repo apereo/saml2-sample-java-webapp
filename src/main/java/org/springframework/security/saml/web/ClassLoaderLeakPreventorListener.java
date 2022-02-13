28eef53bea1686c75a21987ec7f21532bf4f80ea
hdeadman <hal.deadman@gmail.com>
2022-02-13 18:54:23 -0500

    fix logging caused by JUL trying to use bridge not available outside webapp

--- src/main/java/org/springframework/security/saml/web/ClassLoaderLeakPreventorListener.java
+++ src/main/java/org/springframework/security/saml/web/ClassLoaderLeakPreventorListener.java
@@ -3,7 +3,7 @@ package org.springframework.security.saml.web;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 /**
- * Override standard logger (route it to slf4j) so we can control logging.
+ * Extend ClassLoaderLeakPreventorListener to override standard logger (route it to slf4j) so we can control logging.
  * This might risk classloader leak but we just want threads stopped.
  */
 public class ClassLoaderLeakPreventorListener
