d12659de835c7ed275028956494985aff9cc5398
Misagh <mm1844@gmail.com>
2022-04-05 14:46:11 +0400

    resume with docker build

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM adoptopenjdk/openjdk11:alpine-slim AS war
+
+RUN mkdir -p sp-webapp
+COPY ./src sp-webapp/src/
+COPY ./gradle/ sp-webapp/gradle/
+COPY ./gradlew ./build.gradle ./gradle.properties ./docker-run.sh /sp-webapp/
+
+RUN mkdir -p ~/.gradle \
+    && echo "org.gradle.daemon=false" >> ~/.gradle/gradle.properties \
+    && echo "org.gradle.configureondemand=true" >> ~/.gradle/gradle.properties \
+    && cd sp-webapp \
+    && chmod 750 ./gradlew \
+    && ./gradlew --version;
+
+RUN cd sp-webapp && ./gradlew clean build --parallel --no-daemon;
+    
+EXPOSE 9876 8076
+
+ENV PATH $PATH:$JAVA_HOME/bin:.
+
+WORKDIR sp-webapp
+RUN ls -al && chmod +x *.sh
+
+ENTRYPOINT ["docker-run.sh"]
---
+++ docker-run.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+ARGS=""
+if [ -n "${IDP_METADATA}" ]; then
+    ARGS="${ARGS} -DidpMetadata="${IDP_METADATA}""
+fi
+
+if [ -n "${IDP_METADATA_TYPE}" ]; then
+    ARGS="${ARGS} -DidpMetadataType="${IDP_METADATA_TYPE}""
+fi
+
+if [ -n "${METADATA_TYPE}" ]; then
+    ARGS="${ARGS} -DmetadataType="${METADATA_TYPE}""
+fi
+
+if [ -n "${INCLUDE_SCOPING}" ]; then
+    ARGS="${ARGS} -DincludeScoping="${INCLUDE_SCOPING}""
+fi
+
+if [ -n "${METADATA_LOCATION}" ]; then
+    ARGS="${ARGS} -DmetadataLocation="${METADATA_LOCATION}""
+fi
+
+if [ -n "${RELAY_STATE}" ]; then
+    ARGS="${ARGS} -DrelayState="${RELAY_STATE}""
+fi
+
+if [ -n "${NAMEID_FORMAT}" ]; then
+    ARGS="${ARGS} -DnameIdFormat="${NAMEID_FORMAT}""
+fi
+
+if [ -n "${ALLOWED_IDP1}" ]; then
+    ARGS="${ARGS} -DallowedIdP1="${ALLOWED_IDP1}""
+fi
+
+if [ -n "${ACS_URL}" ]; then
+    ARGS="${ARGS} -DacsUrl="${ACS_URL}""
+fi
+
+if [ -n "${SIGN_AUTHN_REQUESTS}" ]; then
+    ARGS="${ARGS} -DsignAuthnRequests="${SIGN_AUTHN_REQUESTS}""
+fi
+
+if [ -n "${FORCED_AUTHN}" ]; then
+    ARGS="${ARGS} -DforcedAuthn="${FORCED_AUTHN}""
+fi
+
+if [ -n "${AUTHN_REQUEST_BINDING_OPTIONAL}" ]; then
+    ARGS="${ARGS} -DauthnRequestBindingOptional="${AUTHN_REQUEST_BINDING_OPTIONAL}""
+fi
+
+if [ -n "${AUTHN_CONTEXT}" ]; then
+    ARGS="${ARGS} -DauthnContext="${AUTHN_CONTEXT}""
+fi
+
+echo "Running SAML2 sample application with ${ARGS}"
+./gradlew jettyRunWar "${ARGS}"
